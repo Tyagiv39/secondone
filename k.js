d360badd9f4b0d044622c403de7f46466be54ef1
Tyagiv39 <68985107+Tyagiv39@users.noreply.github.com>
2020-07-30 15:26:04 +0530

    Add files via upload

---
+++ k.js
@@ -0,0 +1,35 @@
+var cheerio=require('cheerio');
+var request=require('request');
+var mysql=require('mysql');
+var cron=require('node-cron');
+
+var con=mysql.createConnection({
+  host:"localhost",
+  user:"tyagiv39",
+  password:"7910",
+  database:"mydb"
+})
+
+
+con.connect(function(err,result){
+if(err) throw err;
+console.log('connectrd');
+var url='https://in.tradingview.com/markets/stocks-india/market-movers-active/';
+  cron.schedule("*/30 * * * * *",function(){
+     request(url,function(err,resp,body){
+  if (err) throw err;
+  const $=cheerio.load(body);
+  for(var i=0;i<101;i++){
+var a=$('.tv-screener-table__symbol-container ').find('a').eq(i).text();
+var b=$('tbody tr').children('td:nth-child(2)').eq(i).text();
+var sql=  "UPDATE stockdatas SET stockname='"+a+"',LTPprice='"+b+"' WHERE id='"+i+"'";
+//console.log(a +"------" +b);
+con.query(sql,function (err,result) {
+   });
+};});
+con.query("SELECT*FROM stockdatas" ,function (err,result){
+  if (err) throw err;
+  console.log(result); });
+});
+
+});
